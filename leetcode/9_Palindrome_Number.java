3c5442a1c1bd6b2f3dd7ff2c1427900d064c814f
Gourav anand <90522738+venomusblood568@users.noreply.github.com>
2023-05-24 01:42:25 +0530

    9_Palindrome_Number.java

--- leetcode/9_Palindrome_Number.java
+++ leetcode/9_Palindrome_Number.java
@@ -10,3 +10,19 @@ class Solution {
        return original == rev;
     } 
 }
+
+/*
+//easy way 
+//combined all the condition in one place
+class Solution {
+    public boolean isPalindrome(int x) {
+        int k=x;
+        int s=0;
+            while(k>0){
+            s=k%10+s*10;
+            k/=10;
+            }
+        return s==x;
+    }
+}
+*/
