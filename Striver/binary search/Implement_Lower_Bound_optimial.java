783645e4162422cf540f2b7c5a98e8ec162a0249
Gourav anand <90522738+venomusblood568@users.noreply.github.com>
2023-11-02 22:12:46 +0530

    Create Implement_Lower_Bound_optimial.java

---
+++ Striver/binary search/Implement_Lower_Bound_optimial.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public static int lowerBound(int []arr, int n, int x) {
+        int low = 0; 
+        int high = n-1;
+        int ans = n;
+
+        while(low <= high){
+            int mid = (low + high)/2;
+            //maybe an answer 
+            if (arr[mid] >= x){
+                ans = mid;
+              // look for smaller index on the left 
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1; //look on the right 
+            }
+        }
+    return ans;
+    }
+}
