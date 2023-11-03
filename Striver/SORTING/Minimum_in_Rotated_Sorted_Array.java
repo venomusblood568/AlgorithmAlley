c0d16cdf72c18564bd4ea43f2959a1e66f25d410
Gourav anand <90522738+venomusblood568@users.noreply.github.com>
2023-11-03 21:46:08 +0530

    Create Minimum_in_Rotated_Sorted_Array.java

---
+++ Striver/SORTING/Minimum_in_Rotated_Sorted_Array.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public static int findMin(int []arr) {
+        int low = 0;
+        int high = arr.length -1;
+        int ans = Integer.MAX_VALUE;
+
+        while(low <= high){
+            int mid = (low + high)/2;
+            if(arr[low] <= arr[high]){
+                ans = Math.min(ans,arr[low]);
+                break;
+            }
+            if (arr[low] <= arr[mid]){
+                ans = Math.min(ans,arr[low]);
+                low = mid +1;
+            }
+            else {
+                ans = Math.min(ans, arr[mid]);
+                high = mid -1;
+
+            }
+        }
+        return ans;
+    }
+}
